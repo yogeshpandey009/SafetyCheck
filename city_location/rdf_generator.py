8afe634f789aa40658da01a19337a6f906cc9929
Harish Malik <hmalik2@asu.edu>
2015-11-24 18:17:54 -0700

    Adding doctype

--- city_location/rdf_generator.py
+++ city_location/rdf_generator.py
@@ -4,10 +4,16 @@ import csv
 
 rdf = open("city_rdf", "wb")
 try:
+    rdf.write("<?xml version=\"1.0\"?>\n")
+    rdf.write("<!DOCTYPE rdf:RDF [\n")
+    rdf.write("	<!ENTITY xsd \"http://www.w3.org/2001/XMLSchema#\" >\n")
+    rdf.write("	<!ENTITY rdfs \"http://www.w3.org/2000/01/rdf-schema#\" >\n")
+    rdf.write("	<!ENTITY rdf \"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" >\n")
+    rdf.write("	<!ENTITY sc \"http://www.semanticweb.org/ontologies/2015/10/SafetyCheck#\" >\n")
+    rdf.write("]>\n")
     rdf.write("<rdf:RDF\n")
     rdf.write("	xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" \n")
     rdf.write("	xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" \n")
-    rdf.write("	xmlns:owl=\"http://www.w3.org/2002/07/owl#\" \n")
     rdf.write("	xmlns:xsd=\"http://www.w3.org/2001/XMLSchema#\" \n")
     rdf.write("	xmlns:sc=\"http://www.semanticweb.org/ontologies/2015/10/SafetyCheck#\">\n\n")
 
@@ -17,12 +23,13 @@ try:
         reader = csv.reader(f)
         x = 1
         for row in reader:
-            rdf.write("	<rdfd:Description rdf:about=\"&sc;city" + str(x) + "\">\n")
-            rdf.write("		<sc:city rdf:datatype=\"&xsd;string\">" + row[0] + "</sc:city>\n")
-            rdf.write("		<sc:atLatitude rdf:datatype=\"&xsd;float\">" + row[1] + "</sc:atLatitude>\n")
-            rdf.write("		<sc:atLongitude rdf:datatype=\"&xsd;float\">" + row[2] + "</sc:atLongitude>\n")
-            rdf.write("	</rdf:Description>\n\n")
-            x = x+1
+            if row[0] != 'city_name':
+                rdf.write("	<rdf:Description rdf:about=\"&sc;city" + str(x) + "\">\n")
+                rdf.write("		<sc:hasLocationName rdf:datatype=\"&xsd;string\">" + row[0] + "</sc:hasLocationName>\n")
+                rdf.write("		<sc:hasLatitude rdf:datatype=\"&xsd;float\">" + row[1] + "</sc:hasLatitude>\n")
+                rdf.write("		<sc:hasLongitude rdf:datatype=\"&xsd;float\">" + row[2] + "</sc:hasLongitude>\n")
+                rdf.write("	</rdf:Description>\n\n")
+                x = x+1
 
     rdf.write("</rdf:RDF>")
 
